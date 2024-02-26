3ad532c13af2accf302c0396c998651db5e4cf59
Miguel <miguel.scaccia1@gmail.com>
2024-02-26 11:08:21 -0300

    Acomodado de estilos de cards

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -21,27 +21,23 @@ export default function Cards({ drinks }) {
     }
   };
   return (
-    <div className="sm:w-11/12 md:w-9/12 lg:w-8/12 xl:w-7/12 mx-auto mt-5">
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+    <div className="sm:w-11/12 md:w-9/12 lg:w-8/12 xl:w-7/12 mx-auto mt-5 mb-5">
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 ">
         {drinks.map((card) => (
           <div
             key={card.id}
-            className="bg-white rounded-lg shadow-lg overflow-hidden w-[90%] mx-auto"
+            className="bg-white rounded-lg shadow-lg overflow-hidden "
           >
-            <div className="h-60 bg-cover bg-center flex items-center justify-center bg-slate-400">
-              <img
-                src={card.image}
-                alt={card.name}
-                className="w-22 h-[25vh] flex "
-              />
-            </div>
-            <div className="p-4">
-              <h2 className="text-xl font-bold mb-2 text-black">{card.name}</h2>
-              <p className="text-gray-500 text-sm">Stock: {card.stock}</p>
-              <p className="text-green-600 font-semibold text-lg mt-2">
-                ${card.price}
-              </p>
-            </div>
+             <div className="h-60 bg-cover bg-center flex items-center justify-center bg-slate-400">
+            <img src={card.image} alt={card.name} className="w-22 h-[25vh] flex" />
+          </div>
+          <div className="p-4">
+            <h2 className="text-xl font-bold mb-2 text-black">{card.name}</h2>
+            <p className="text-gray-500 text-sm">Stock: {card.stock}</p>
+            <p className="text-green-600 font-semibold text-lg mt-2">
+              ${card.price}
+            </p>
+          </div>
             <div className="w-full h-10 bg-green-500 flex items-center justify-center">
               <button
                 onClick={() => handleAddToCart(card)}
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -3,15 +3,14 @@ import Cards from "../Cards/Cards";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllDrinks } from "../../redux/actions";
 
-
 export default function Home() {
   const [pageSize, setPageSize] = useState(6);
   const [currentPage, setCurrentPage] = useState(1);
-  const [isSmall, setIsSmall] = useState(false)
-  console.log(setPageSize)
+  const [isSmall, setIsSmall] = useState(false);
+  console.log(setPageSize);
   const dispatch = useDispatch();
   const drinks = useSelector((state) => state.drinks);
-  
+
   useEffect(() => {
     dispatch(getAllDrinks());
   }, [dispatch]);
@@ -48,7 +47,7 @@ export default function Home() {
   const paginatedDrinks = drinks.slice(startIndex, endIndex);
 
   return (
-    <div className="bg-cover bg-center w-full h-screen relative flex flex-col items-center">
+    <div className="bg-cover bg-center w-full h-auto relative flex flex-col items-center">
       <div className="flex justify-center items-center h-1/4 sm:h-1/3 md:h-1/4 lg:h-1/5">
         <img
           src="https://res.cloudinary.com/duclhjrri/image/upload/v1695577547/Dise%C3%B1o_sin_t%C3%ADtulo-removebg_kx4uab.png"
@@ -59,55 +58,54 @@ export default function Home() {
       <div className="w-11/12 sm:w-10/12 md:w-9/12 lg:w-8/12 xl:w-7/12 mx-auto sm:h-[10vh] md:h-[10vh] lg:h-[10vh] bg-slate-900 shadow-lg shadow-black mt-5">
         <div className="flex bottom-0 left-0 w-full h-auto p-4">
           <h2 className="text-center sm:text-md md:text-lg lg:text-xl text-white">
-            ¡Bienvenido a nuestra página! Podrán reservar con anticipación su bebida
-            para la fecha que tengamos disponible en el momento.
+            ¡Bienvenido a nuestra página! Podrán reservar con anticipación su
+            bebida para la fecha que tengamos disponible en el momento.
           </h2>
         </div>
       </div>
       {isSmall ? (
         <>
-        <div className=" mt-[7vh]">
-        <Cards drinks={paginatedDrinks} />
-      </div>
-      <div className="flex justify-center mt-4 h-44">
-        <button
-          onClick={() => setCurrentPage(currentPage - 1)}
-          disabled={currentPage === 1}
-          className="mr-4 bg-blue-600 h-12 w-20 rounded-lg"
-        >
-          Anterior
-        </button>
-        <button
-          onClick={() => setCurrentPage(currentPage + 1)}
-          disabled={endIndex >= drinks.length}
-          className="mr-4 bg-blue-600 h-12 w-20 rounded-lg"
-        >
-          Siguiente
-        </button>
-      </div>
+          <div className=" mt-[7vh]">
+            <Cards drinks={paginatedDrinks} />
+          </div>
+          <div className="flex-grow mt-4 flex items-center ml-5">
+            <button
+              onClick={() => setCurrentPage(currentPage - 1)}
+              disabled={currentPage === 1}
+              className="mr-4 bg-blue-600 h-12 w-20 rounded-lg"
+            >
+              Anterior
+            </button>
+            <button
+              onClick={() => setCurrentPage(currentPage + 1)}
+              disabled={endIndex >= drinks.length}
+              className="mr-4 bg-blue-600 h-12 w-20 rounded-lg"
+            >
+              Siguiente
+            </button>
+          </div>
         </>
-      ): (
+      ) : (
         <>
           <div className="flex-grow mt-4 flex items-center w-[80%] mx-auto">
-        <button
-          onClick={() => setCurrentPage(currentPage - 1)}
-          disabled={currentPage === 1}
-          className="bg-blue-600 h-12 w-20 rounded-lg"
-        >
-          Anterior
-        </button>
-        <Cards drinks={paginatedDrinks} />
-        <button
-          onClick={() => setCurrentPage(currentPage + 1)}
-          disabled={endIndex >= drinks.length}
-          className="bg-blue-600 h-12 w-20 rounded-lg"
-        >
-          Siguiente
-        </button>
-      </div>
+            <button
+              onClick={() => setCurrentPage(currentPage - 1)}
+              disabled={currentPage === 1}
+              className="bg-blue-600 h-12 w-20 rounded-lg"
+            >
+              Anterior
+            </button>
+            <Cards drinks={paginatedDrinks} />
+            <button
+              onClick={() => setCurrentPage(currentPage + 1)}
+              disabled={endIndex >= drinks.length}
+              className="bg-blue-600 h-12 w-20 rounded-lg"
+            >
+              Siguiente
+            </button>
+          </div>
         </>
       )}
-      
     </div>
   );
 }
