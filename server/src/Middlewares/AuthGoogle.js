5528e28c776c6a4e6460bc8c07715a47f9c3fee3
Miguel <miguel.scaccia1@gmail.com>
2024-03-22 00:18:52 -0300

    prueba

--- server/src/Middlewares/AuthGoogle.js
+++ server/src/Middlewares/AuthGoogle.js
@@ -1,19 +1,29 @@
-const admin = require("../ConfigGoogleAuth/firebaseAdmin")
+const admin = require("../ConfigGoogleAuth/firebaseAdmin");
 
 const decodeToken = async (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(' ')[1];
-        const decodeValue = await admin.auth().verifyIdToken(token);
-        
-        if (decodeValue) {
-            req.user = decodeValue;
-            return next();
+        // Verificar si se proporciona un token de autorización en la solicitud
+        if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
+            return res.status(401).json({ message: "No se proporcionó un token de autorización" });
         }
 
-        return res.json({message: "Sin permiso"});
+        // Extraer el token de autorización y decodificarlo
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = await admin.auth().verifyIdToken(token);
 
+        // Verificar si el token es válido
+        if (!decodedToken) {
+            return res.status(401).json({ message: "Token de autorización no válido" });
+        }
+
+        // Adjuntar la información del usuario decodificado al objeto req
+        req.user = decodedToken;
+
+        // Continuar con el siguiente middleware o controlador de ruta
+        next();
     } catch (error) {
-        return res.json({message: "Error en el servidor"});
+        console.error("Error al decodificar el token de autorización:", error);
+        return res.status(500).json({ message: "Error en el servidor al decodificar el token de autorización" });
     }
 };
 
